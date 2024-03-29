8b711f16630881752bea2d5689ccbac8ea0e7af3
Jitendra7653 <96977015+Jitendra7653@users.noreply.github.com>
2024-03-29 20:20:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ echo "Cloning Repo...."
 git clone https://github.com/Jitendra7653/Adlinkfly-bot /Adlinkfly-bot
 cd /Adlinkfly-bot
 echo "Starting Bot.... Please Wait."
-python3 bot.py
+python3 main.py
