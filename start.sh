d4cfb6f90e7d8fa0f62388391c91d22bacf394d1
Jitendra7653 <96977015+Jitendra7653@users.noreply.github.com>
2024-03-29 20:00:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,5 @@
-if [ -z $SOURCE_CODE ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/Jitendra7653/Adlinkfly-bot /Adlinkfly-bot
-else
-  echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /Adlinkfly-bot
-fi
+echo "Cloning Repo...."
+git clone https://github.com/Jitendra7653/Adlinkfly-bot /Adlinkfly-bot
 cd /Adlinkfly-bot
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
-python3 main.py
+echo "Starting Bot.... Please Wait."
+python3 bot.py
