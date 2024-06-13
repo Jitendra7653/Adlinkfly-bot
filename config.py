de1c2ebaa2d38f8996ce4c0421eff49643219741
Jitendra7653 <96977015+Jitendra7653@users.noreply.github.com>
2024-06-13 10:56:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,40 +16,38 @@ def is_enabled(value, default):
 
 # Mandatory variables for the bot to start
 # API ID from https://my.telegram.org/auth
-API_ID = int(os.environ.get("API_ID"))
+API_ID = int(os.environ.get("API_ID","29253270"))
 # API Hash from https://my.telegram.org/auth
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")  # Bot token from @BotFather
+API_HASH = os.environ.get("API_HASH","affd2bec1baa53bf410fd9ff05eff463")
+BOT_TOKEN = os.environ.get("BOT_TOKEN","")  # Bot token from @BotFather
 ADMINS = (
-    [int(i.strip()) for i in os.environ.get("ADMINS").split(",")]
-    if os.environ.get("ADMINS")
+    [int(i.strip()) for i in os.environ.get("ADMINS","1731432915").split(",")]
+    if os.environ.get("ADMINS","1731432915")
     else []
 )
 
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "MdiskConvertor")
+DATABASE_NAME = os.environ.get("DATABASE_NAME","Cluster0")
 DATABASE_URL = os.environ.get(
-    "DATABASE_URL", None
-)  # mongodb uri from https://www.mongodb.com/
-OWNER_ID = int(os.environ.get("OWNER_ID"))  # id of the owner
+    "DATABASE_URL","mongodb+srv://chandra7653p:oei42rGCHV9JCqgq@cluster0.pw5huiu.mongodb.net/?retryWrites=true&w=majority")  # mongodb uri from https://www.mongodb.com/
+OWNER_ID = int(os.environ.get("OWNER_ID","1731432915"))  # id of the owner
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 
 #  Optionnal variables
 LOG_CHANNEL = int(
-    os.environ.get("LOG_CHANNEL", "0")
+    os.environ.get("LOG_CHANNEL", "-1002126197074")
 )  # log channel for information about users
 UPDATE_CHANNEL = int(os.environ.get(
-    "UPDATE_CHANNEL", False))  # For Force Subscription
+    "UPDATE_CHANNEL","https://t.me/all_in_1_tricks"))  # For Force Subscription
 BROADCAST_AS_COPY = is_enabled(
     (os.environ.get("BROADCAST_AS_COPY", "False")), False
 )  # true if forward should be avoided
 IS_PRIVATE = is_enabled(
-    os.environ.get("IS_PRIVATE", "False"), "False"
-)  # true for private use and restricting users
+    os.environ.get("IS_PRIVATE", "True"))  # true for private use and restricting users
 SOURCE_CODE = os.environ.get(
-    "SOURCE_CODE", "https://github.com/Jitendra7653/Adlinkfly"
+    "SOURCE_CODE", "https://allin1tricks.online"
 )  # for upstream repo
 # image when someone hit /start
-WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", "")
+WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", "https://telegra.ph/file/baa36936c1cd55044aa0a.jpg")
 LINK_BYPASS = is_enabled(
     (os.environ.get("LINK_BYPASS", "False")), False
 )  # if true, urls will be bypassed
@@ -78,4 +76,4 @@ FORWARD_MESSAGE  = is_enabled(
 
 WEB_SERVER = is_enabled(os.environ.get("WEB_SERVER", "True"), True)
 PING_INTERVAL = int(os.environ.get("PING_INTERVAL", "240"))
-PORT = int(os.environ.get("PORT", ""))
+PORT = int(os.environ.get("PORT", "8080"))
