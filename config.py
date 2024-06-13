ac6731178b81fb0035499f138b84cf8c18874fce
Jitendra7653 <96977015+Jitendra7653@users.noreply.github.com>
2024-06-13 11:22:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
-
 
 def is_enabled(value, default):
     if value.lower() in ["true", "yes", "1", "enable", "y"]:
@@ -13,49 +11,38 @@ def is_enabled(value, default):
     else:
         return default
 
-
 # Mandatory variables for the bot to start
 # API ID from https://my.telegram.org/auth
-API_ID = int(os.environ.get("API_ID","29253270"))
+API_ID = int(os.environ.get("API_ID", "29253270"))
 # API Hash from https://my.telegram.org/auth
-API_HASH = os.environ.get("API_HASH","affd2bec1baa53bf410fd9ff05eff463")
-BOT_TOKEN = os.environ.get("BOT_TOKEN","")  # Bot token from @BotFather
+API_HASH = os.environ.get("API_HASH", "affd2bec1baa53bf410fd9ff05eff463")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")  # Bot token from @BotFather
 ADMINS = (
-    [int(i.strip()) for i in os.environ.get("ADMINS","1731432915").split(",")]
-    if os.environ.get("ADMINS","1731432915")
+    [int(i.strip()) for i in os.environ.get("ADMINS", "1731432915").split(",")]
+    if os.environ.get("ADMINS", "1731432915")
     else []
 )
 
-DATABASE_NAME = os.environ.get("DATABASE_NAME","Cluster0")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 DATABASE_URL = os.environ.get(
-    "DATABASE_URL","mongodb+srv://chandra7653p:oei42rGCHV9JCqgq@cluster0.pw5huiu.mongodb.net/?retryWrites=true&w=majority")  # mongodb uri from https://www.mongodb.com/
-OWNER_ID = int(os.environ.get("OWNER_ID","1731432915"))  # id of the owner
+    "DATABASE_URL", "mongodb+srv://chandra7653p:oei42rGCHV9JCqgq@cluster0.pw5huiu.mongodb.net/?retryWrites=true&w=majority")  # mongodb uri from https://www.mongodb.com/
+OWNER_ID = int(os.environ.get("OWNER_ID", "1731432915"))  # id of the owner
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 
-#  Optionnal variables
-LOG_CHANNEL = int(
-    os.environ.get("LOG_CHANNEL", "-1002126197074")
-)  # log channel for information about users
-UPDATE_CHANNEL = int(os.environ.get(
-    "UPDATE_CHANNEL","-1001976505683"))  # For Force Subscription
-BROADCAST_AS_COPY = is_enabled(
-    (os.environ.get("BROADCAST_AS_COPY", "False")), False
-)  # true if forward should be avoided
-IS_PRIVATE = is_enabled(
-    os.environ.get("IS_PRIVATE", "true"))  # true for private use and restricting users
-SOURCE_CODE = os.environ.get(
-    "SOURCE_CODE", "https://allin1tricks.online"
-)  # for upstream repo
+# Optional variables
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002126197074"))  # log channel for information about users
+UPDATE_CHANNEL = int(os.environ.get("UPDATE_CHANNEL", "-1001976505683"))  # For Force Subscription
+BROADCAST_AS_COPY = is_enabled(os.environ.get("BROADCAST_AS_COPY", "False"), False)  # true if forward should be avoided
+IS_PRIVATE = is_enabled(os.environ.get("IS_PRIVATE", "true"), True)  # true for private use and restricting users
+SOURCE_CODE = os.environ.get("SOURCE_CODE", "https://allin1tricks.online")  # for upstream repo
 # image when someone hit /start
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", "https://telegra.ph/file/baa36936c1cd55044aa0a.jpg")
-LINK_BYPASS = is_enabled(
-    (os.environ.get("LINK_BYPASS", "False")), False
-)  # if true, urls will be bypassed
+LINK_BYPASS = is_enabled(os.environ.get("LINK_BYPASS", "False"), False)  # if true, urls will be bypassed
 # your shortener site domain
 BASE_SITE = os.environ.get("BASE_SITE", "mxlinks.cloud")
 
 # For Admin use
-CHANNELS = is_enabled((os.environ.get("CHANNELS", "True")), True)
+CHANNELS = is_enabled(os.environ.get("CHANNELS", "True"), True)
 CHANNEL_ID = (
     [int(i.strip()) for i in os.environ.get("CHANNEL_ID").split(" ")]
     if os.environ.get("CHANNEL_ID")
@@ -69,10 +56,7 @@ DE_BYPASS = (
 )
 DE_BYPASS.append("mdisk.me")
 
-FORWARD_MESSAGE  = is_enabled(
-    (os.environ.get("FORWARD_MESSAGE", "False")), False
-)  # true if forwardd message to converted by reposting the post
-
+FORWARD_MESSAGE = is_enabled(os.environ.get("FORWARD_MESSAGE", "False"), False)  # true if forward message to converted by reposting the post
 
 WEB_SERVER = is_enabled(os.environ.get("WEB_SERVER", "True"), True)
 PING_INTERVAL = int(os.environ.get("PING_INTERVAL", "240"))
